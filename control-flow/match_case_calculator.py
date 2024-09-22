95ed53afddc3a507ea9969c704b455e341e9ccaf
Mustapha Khamiz <mr.khamizmustapha@gmail.com>
2024-09-22 22:51:31 +0100

    Create match_case_calculator.py

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,5 @@
+# match_case_calculator.py
+# Prompt the user to enter two numbers
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+# Prompt the user to choose an operation
