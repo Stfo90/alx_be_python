433d685fd9324945cbfbe7bb12cd4459ce4b638e
Mustapha Khamiz <mr.khamizmustapha@gmail.com>
2024-09-22 21:23:03 +0100

    Add files via upload
    calculate interest

---
+++ simple_interest.py
@@ -0,0 +1,8 @@
+# Define principle, rate, and time with values 1000, 0.05 and 3 respectively
+principle = 1000
+rate = 0.05
+time = 3
+# Calculate the simple interest using the formula I = P * R * T
+interest = principle * rate * time
+# Print the calculated interest in the specified format
+print("The simple interest is:", interest)
