0397de53efcca75c0e5f8f62a7bc92dc828326b3
Mustapha Khamiz <mr.khamizmustapha@gmail.com>
2024-09-22 23:05:44 +0100

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,10 +1,7 @@
-# Define principle, rate, and time with values 1000, 0.05 and 3 respectively
-principle =1000
-rate =0.05
-time =3
+principal = 1000 
+rate = 0.05 
+time=3
 
-# Calculate the simple interest using the formula I = P * R * T
-interest = principle * rate * time
+interest = principal * rate * time
 
-# Print the calculated interest in the specified format
-print("The simple interest is:", interest)
+print(f"The simple interest is:",interest)
